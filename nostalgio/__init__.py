4bdf817c3ebc149d7d6c060af49bd0f4c4725920
Josh <josh@halfnibble.com>
2016-08-17 01:05:55 -0700

    Remove celery.

--- nostalgio/__init__.py
+++ nostalgio/__init__.py
@@ -5,4 +5,4 @@ from __future__ import absolute_import
 # Django starts so that tasks can use this celery app.
 # Without this Django wouldn't know which celery app to use.
 # See http://celery.readthedocs.org/en/latest/django/first-steps-with-django.html
-from .celery import app as celery_app
+# from .celery import app as celery_app
